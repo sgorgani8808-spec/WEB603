b2bb48254eab5dd24e498ec3a001abc1b80aa714
sgorgani8808-spec <s.gorgani.8808@westcliff.edu>
2026-03-09 00:34:36 -0700

    W1-D3 Lab React shopping cart

--- W1-D2 Demo/demo3.js
+++ W1-D2 Demo/demo3.js
@@ -1,5 +1,11 @@
 //functional example and adding Events
 
-  
-  
-  ReactDOM.render(myelement, document.getElementById('root'));
+function clickbutton() {
+  alert("Clicked!!!!!!");
+}
+
+const myelement = (
+  <button onClick={clickbutton}>Click the button!</button>
+);
+
+ReactDOM.render(myelement, document.getElementById('root'));
